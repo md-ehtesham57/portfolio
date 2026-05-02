d5245dc3b86bf343197e6c9e3c16dd1497557831
md-ehtesham57 <mdehtesham313@gmail.com>
2026-05-02 23:05:23 +0530

    Added all the routes in homepage as well to make it look scrollable.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: [
+    "ordinance-gardens-rebecca-rabbit.trycloudflare.com"
+  ],
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Hero from "@/components/sections/Hero";
+import ProjectsSection from "@/components/sections/ProjectsSection";
+import AboutSection from "@/components/sections/AboutSection";
+import ContactSection from "@/components/sections/ContactSection";
 
 export default function Home() {
   return (
-    <main>
+    <>
       <Hero />
-    </main>
+      <ProjectsSection />
+      <AboutSection />
+      <ContactSection />
+    </>
   );
 }
